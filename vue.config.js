e9322a7371ff3eea2eb8a7e374098971b7d2c24b
LiuJiangshan <ljs234@qq.com>
2021-02-23 20:07:30 +0800

    u

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const aliasConfig = require('./alias.config.js')
 const merge = require('webpack-merge')
 module.exports = {
+  publicPath: './',
   configureWebpack: merge(aliasConfig),
   css: { extract: true }
   // transpileDependencies: ['ele-tree-table']
