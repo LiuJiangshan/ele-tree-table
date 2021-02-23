c4d056a768ba411633ef045facf233ad25d151eb
LiuJiangshan <ljs234@qq.com>
2021-02-23 20:12:11 +0800

    u

---
+++ precache-manifest.f714dca7719d83fe8cf05a9e5af673a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9a6a0898b8ce7cf80f0",
+    "url": "css/app.b4777f1a.css"
+  },
+  {
+    "revision": "b9a6a0898b8ce7cf80f0",
+    "url": "js/app.8cf32df4.js"
+  },
+  {
+    "revision": "f91ae5a9315c3e66d2c3",
+    "url": "css/chunk-vendors.455ad7bb.css"
+  },
+  {
+    "revision": "f91ae5a9315c3e66d2c3",
+    "url": "js/chunk-vendors.6048f0da.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "e11ec47b5b2964f75313343ccaa98c11",
+    "url": "index.html"
+  }
+];
